f8d1d70ae674cdef68b6d1a8d7220a72f9d9153b
joao-wess <joaovwcosta3@gmail.com>
2026-09-22 14:51:10 -0300

    feat: implement core mathematical operations in CalculadoraService

---
+++ src/com/joaovitor/calculadora/service/CalculadoraService.java
@@ -0,0 +1,20 @@
+package com.joaovitor.calculadora.service;
+
+public class CalculadoraService {
+  //methods
+  public double addition(double firstNumber, double secondNumber) {
+    return firstNumber + secondNumber;
+  }
+
+  public double subtraction(double firstNumber, double secondNumber) {
+    return firstNumber - secondNumber;
+  }
+
+  public double division(double firstNumber, double secondNumber) {
+    return firstNumber / secondNumber;
+  }
+
+  public double multiplication(double firstNumber, double secondNumber) {
+    return firstNumber * secondNumber;
+  }
+}
